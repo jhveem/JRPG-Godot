677cf12aa4f249e0365532b919c92e4c6feb9620
Joshua Hveem <joshua.hveem@gmail.com>
2021-02-21 00:03:52 -0700

    tophat as sprite

---
+++ battle/characters/players/Tophat.gd
@@ -0,0 +1,12 @@
+extends "res://battle/characters/BaseCharacter.gd" 
+
+func _ready():
+    stats = {
+        'health': 80,
+        'mana': 10,
+        'strength': 8,
+        'magic': 1,
+        'defense': 5,
+        'speed': 4
+    }
+    ._ready()
